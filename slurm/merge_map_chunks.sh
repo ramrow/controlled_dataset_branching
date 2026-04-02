2b9543c9c360f8653ea26ec74d291d84a285badc
ramrow <peij.xu0@gmail.com>
2026-04-01 23:48:41 -0400

    right path

--- slurm/merge_map_chunks.sh
+++ slurm/merge_map_chunks.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -e
-cd /mnt/home/DDN_Copy/rpi/pxu10/dataset_branching
+cd /mnt/lustre/rpi/pxu10/branching
 python scripts/merge_case_maps.py --inputs case_tutorial_map_chunk_0.json case_tutorial_map_chunk_1.json case_tutorial_map_chunk_2.json case_tutorial_map_chunk_3.json --out case_tutorial_map.json
+
